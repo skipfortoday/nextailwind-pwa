639532ffae906e0dc50554685fe8afae3d13f79b
Rizqi Pratama <rev4rmy@gmail.com>
2021-09-05 14:35:07 +0700

    menambahkamn font tailwind

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,10 @@ class MyDocument extends Document {
 					<link rel="manifest" href="/manifest.json" />
 					<link rel="apple-touch-icon" href="/logo-128x128.png" />
 					<link rel="theme-color" href="#fff" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+						rel="stylesheet"
+					/>
 				</Head>
 				<body>
 					<Main />
